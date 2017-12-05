d3ce63192f28b00a488830770bba146236b06e1c
Brad Hambrice <hambrice@uab.edu>
2017-12-05 00:09:39 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,7 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n"
+  puts "-----------\n"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} \n"
+  puts "-----------\n"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} \n"
+end
