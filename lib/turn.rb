9b086ee0309ad19c9e016d8aeec74f4900b0ec0e
Brad Hambrice <hambrice@uab.edu>
2017-12-05 00:14:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,28 @@ def display_board(board)
   puts "-----------\n"
   puts " #{board[6]} | #{board[7]} | #{board[8]} \n"
 end
+def valid_move? (board, index)
+  if index < 9 && index >= 0 && !position_taken?(board,index)
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def position_taken? (board, index)
+  if board[index] == "X" || board[index] == "O"
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def move (board, index, token="X")
+  board[index] = token
+end
+
+def turn 
+  puts "Please enter 1-9:"
+  input = gets.String
+  
+end
